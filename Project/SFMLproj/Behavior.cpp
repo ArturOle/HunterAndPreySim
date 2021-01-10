73815a9eb31590c9f1c9bf1f2679f4f5e6cc9e50
ArturOle <artur.oleksinski99@gmail.com>
2021-01-10 12:05:31 +0100

    Refractoring

--- Project/SFMLproj/Behavior.cpp
+++ Project/SFMLproj/Behavior.cpp
@@ -15,14 +15,16 @@ sf::Vector2f Behavior::Repulsion(float x_first_in, float y_first_in, float x_sec
 }
 
 
-sf::Vector2f Behavior::Attraction(float x_first_in, float y_first_in, float x_second_in, float y_second_in)
+sf::Vector2f Behavior::Attraction(float x_first_in, float y_first_in, float x_second_in, float y_second_in)		  
 {
 	float x_move = x_second_in - x_first_in;
 	float y_move = y_second_in - y_first_in;
 
 	float current_distance_food = sqrtf(pow(x_move, 2) + pow(y_move, 2));
 
-	sf::Vector2f directional_vector = { x_move / current_distance_food,y_move / current_distance_food };
+	sf::Vector2f directional_vector = { x_move / current_distance_food,
+										y_move / current_distance_food };
+
 
 	return directional_vector;
 }
